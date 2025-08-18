174a422e925f42c9ca2981b090daa0a138b28a14
DESKTOP-12PLBUK\Lenovo <truonganhkietdev@gmail.com>
2025-08-18 21:43:27 +0700

    remove folder

--- vns-be/BLL/DTOs/AuthenStatusDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BLL.DTOs
-{
-    public class AuthenStatusDto
-    {
-        public required bool Status { get; set; }
-        public required string StatusMessage { get; set; }
-        public string? Token { get; set; }
-    }
-}
