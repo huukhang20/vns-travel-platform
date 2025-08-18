285d48f6377cdb44b21a1f66eb52c7b6209c81b8
DESKTOP-12PLBUK\Lenovo <truonganhkietdev@gmail.com>
2025-08-18 21:38:42 +0700

    update register

--- vns-be/BLL/DTOs/RegisterDto.cs
+++ vns-be/BLL/DTOs/RegisterDto.cs
@@ -5,7 +5,6 @@
         public required string Email { get; set; }
         public required string PasswordHash { get; set; }
         public required string FullName { get; set; }
-        public string? AvatarUrl { get; set; }
         public required string PhoneNumber { get; set; }            
     }
 }
